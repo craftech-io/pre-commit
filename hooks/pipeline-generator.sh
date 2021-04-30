fb15f8158d9fa496e8461efdb47c1a153dadc5ea
Edgardo Izquierdo <zurdo@craftech.io>
2021-04-30 12:01:51 -0300

    chore: check condition if variable if set.

--- hooks/pipeline-generator.sh
+++ hooks/pipeline-generator.sh
@@ -4,17 +4,18 @@
 # Install these tools.
 # brew install pre-commit gawk terraform-docs tflint tfsec coreutils checkov terrascan
 
+set -e 
+
 # Install pipeline generator.
 if ! command -v pipeline-generator &> /dev/null
 then
     echo "installing pipeline-generator"
     pip install -U git+ssh://git@github.com/craftech-io/module-ci.git@feature/gitlab-ci-helper#subdirectory=modules/gitlab-ci-helpers/pipeline-generator
-else
-	echo "ok"
 fi
 
 # Clean cache.
 find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
 
 # Generate gitlab-ci.yml
+if [ -z "$EXTRA_KNOW_HOST" ]; then echo "set \$EXTRA_KNOW_HOST variable." && exit 0; fi
 pipeline-generator -i "craftech/ci-tools:iac-tools-latest" -e $EXTRA_KNOW_HOST -o .gitlab-ci.yml -p gitlab
